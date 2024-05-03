e2aa55d36c5935df8e318e24a30ba1f90c73b6a1
Job van Wee <jobvanwee@gmail.com>
2024-05-03 12:44:54 +0200

    Decoder working this time also on git.

--- EX3/quadrature_decoder.v
+++ EX3/quadrature_decoder.v
@@ -1,49 +1,33 @@
-module quadrature_decoder #(parameter CLOCKSPEED = 1_000_000, PULSES_PER_ROTATION = 2000, COUNTERCLOCKWISE = 'b 1, CLOCKWISE = 'b 0) (
-    clock,
-    // reset,
-
+module quadrature_decoder #(parameter CLOCKSPEED = 1_000_000, PULSES_PER_ROTATION = 2000) (
+    reset,
     input_A,
     input_B,
-    RPMS,
-    direction
+    direction_count
 );
 
-input clock;
+input reset;
 
 input input_A;
 input input_B;
-output integer RPMS = 0;
-output reg direction = 0;
+output integer direction_count;
 
-integer clk_count = 0;
-integer pulse_count = 0;
-reg reset_pulse_count = 0;
+reg reset_reg = 0;
+
+always @ (posedge reset)
+    reset_reg = 1;
 
 always @ (posedge input_A) begin
-    // if B is already high:
-        if (input_B)
-            direction = CLOCKWISE;
 
-    // if B is not already high:
-        else
-            direction = COUNTERCLOCKWISE;
-
-    // counter for input_A
-    pulse_count = pulse_count + 1;
-    if (reset_pulse_count)
-        pulse_count = 1;
+    if (reset_reg) begin
+        direction_count = 0;
+        reset_reg = 0;
+    end else if (input_B)
+        // if B is already high:
+        direction_count = direction_count + 1;
+    else
+        // if B is not already high:
+        direction_count = direction_count - 1;
 end
 
-always @ (posedge clock) begin
-    clk_count = clk_count + 1;
-    
-
-    if (clk_count == CLOCKSPEED) begin
-        RPMS = pulse_count / (PULSES_PER_ROTATION/1000);
-        clk_count = 0;
-        reset_pulse_count = 1;
-    end else if (pulse_count == 1)
-        reset_pulse_count = 0;
-end
 
 endmodule
--- EX3/testbench.v
+++ EX3/testbench.v
@@ -1,75 +1,48 @@
-    module testbench;
-
-`timescale 1ns/1ns  
+module testbench; 
 
 parameter CLOCKWISE   = 'd 0;
 parameter COUNTERCLOCKWISE = 'd 1;
+parameter SIM_END = 22;
+parameter PERIOD = 4;
 
-parameter PULSES_PER_ROTATION = 2000;   // should be power of 2 in order to save division.
-parameter CLOCKSPEED = 1_000_000;       // hz/period
-parameter SIM_END = 10 * CLOCKSPEED + 1;// clock cycles
-
-parameter PERIOD = 2;
-
-// log testbench values
-// integer fd;
-// initial begin
-//   fd = $fopen("log.txt","w");
-// end
-
-// initial rotation speed
-integer cycles_per_pulse = CLOCKSPEED; // Simulate a speed of 0 by having no clock pulses in the span of a second.
-// initial direction
-reg expected_direction = CLOCKWISE;
-
-// instantiate the quadrature decoder module
-reg clk = 0;
+reg [7:0] sim_pulse_count = 0;
 
 reg input_A = 0;
 reg input_B = 0;
-wire [31:0] RPS;
-wire direction;
+reg reset = 0;
 
-quadrature_decoder DUT (clk, input_A, input_B, RPS, direction);
+wire [31:0] direction_count;
 
-// generate the clock
-always begin clk = #(PERIOD/2)~clk; end
+quadrature_decoder DUT (reset, input_A, input_B, direction_count);
 
 // begin of simulation
 initial begin
     $display(" >>> Simulation starting");
-    clk = 0;
+    reset = 1;
+    #1
+    reset = 0;
 end
 
-// called every CLOCKSPEED interval
-always begin #(CLOCKSPEED * PERIOD)
-    // $display(" >>> Simulation existing");
-    // print the RPS and direction
-    //$fwrite(fd, "RPS=%d, direction=%b\n", RPS, direction);
-    $display("RPμS=%d, direction=%b", RPS, direction);
-    // double the rotation speed
-    cycles_per_pulse = cycles_per_pulse / 2;
-
-    // reverse the direction
-    expected_direction=~expected_direction;
-end
-
-// end of simulation
-initial begin #(SIM_END * PERIOD) // total number of clock cycles to simulate
+initial begin #((SIM_END * (PERIOD + 2))) // total number of clock cycles to simulate
     $display(" >>> Simulation finished");
     $dumpfile("quadrature_decoder_output.vcd");
     $dumpvars;
     $finish;
 end
 
+always begin #(PERIOD + 2)
+    // print the current direction count value
+    $display("direction_count=%d", direction_count);
+    sim_pulse_count = sim_pulse_count + 1;
+end
+
 // implement A and B direction logic
 integer clk_counter = 0;
 reg temp1 = 0, temp2 = 0; // Only used in the next procedural block
 
-always begin #PERIOD
-    
+always begin #1
     // toggle every half period
-    if (clk_counter >= cycles_per_pulse / 2) begin
+    if (clk_counter >= PERIOD / 2) begin
         temp2=~temp2;
         clk_counter = 0;
     end else begin
@@ -77,16 +50,16 @@ always begin #PERIOD
     end
 
     // 90 degree delay
-    if (clk_counter == cycles_per_pulse / 4)
+    if (clk_counter == PERIOD / 4)
         temp1=~temp1;
-
-    // assign the correct wire to the correct input
-    if (expected_direction==CLOCKWISE) begin
-        input_A = temp1;
-        input_B = temp2;
-    end else begin
+    
+    // reverse the direction halfway through the sim
+    if (sim_pulse_count < SIM_END/2) begin
         input_A = temp2;
         input_B = temp1;
+    end else begin
+        input_A = temp1;
+        input_B = temp2;
     end
 
 end
